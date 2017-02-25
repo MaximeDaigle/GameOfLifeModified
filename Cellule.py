446c445dd2c280bb463934742a9e89af2b613356
MaximeDaigle <maximedaigle95@gmail.com>
2017-02-25 16:35:00 -0500

    Rename cellule.py to Cellule.py

---
+++ Cellule.py
@@ -0,0 +1,4 @@
+class Cellule:
+
+    def __init__(self, contenu="None"):
+        self.contenu = contenu  #contient string 'None' ou un organisme
--- cellule.py
+++
@@ -1,4 +0,0 @@
-class Cellule:
-
-    def __init__(self, contenu="None"):
-        self.contenu = contenu  #contient string 'None' ou un organisme
